f028f2b4fd3153e2520f846c5f79faadac64216d
David Paul Brackin <dp.brackin@gmail.com>
2018-12-08 16:47:42 +0800

    completed game initialization

--- Game.java
+++ Game.java
@@ -5,7 +5,7 @@ public class Game{
   public static final int BLANKTILE = 0;
   public static final int GREENTILE = 1;
   public static final int REDTILE = 2;
-  public static final int NOTTILE = -1;
+  public static final int NOTTILE = 3;
 
   public Game(int col, int row, int color){
 
@@ -20,6 +20,34 @@ public class Game{
       }
     }
 
+    //start config
+    board[row][col] = color;
+    board[row-1][col-1] = color;
+    board[row-1][col+1] = color;
+
+    //opponent color
+    board[row][col-1] = color == Game.GREENTILE ? Game.REDTILE : Game.GREENTILE;
+    board[row][col+1] = color == Game.GREENTILE ? Game.REDTILE : Game.GREENTILE;
+    board[row-2][col] = color == Game.GREENTILE ? Game.REDTILE : Game.GREENTILE;
+
+  }
+
+  @Override
+  public String toString(){
+    String res = "";
+    for(int i = 0; i < board.length; i++){
+      for(int j = 0; j < board[i].length; j++){
+        res += board[i][j] + " ";
+      }
+      res += "\n";
+    }
+
+    return res;
+  }
+
+  public static void main(String[] args) {
+    Game g = new Game(7,4,Game.GREENTILE);
+    System.out.println(g);
   }
 
 }
